21c866a628b846e67b0f959343f7a5194153ef8f
Abdullah Aly <aa4tf@virginia.edu>
2017-11-05 01:52:44 -0400

    added new helper functions setup map downloading script

--- GoogleMapImageLib.py
+++ GoogleMapImageLib.py
@@ -1,16 +1,35 @@
 #download the google map image from url
 import urllib.request
+import requests
 import math
 
-def downloadMapImage(latitude, longitude, key, fileName, width=640, height=640, zoom=17):
+def is_within_city(latitude, longitude, city_name, key):
+    url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + str(latitude) + "," + str(longitude) + "&key=" + key
+    response = requests.get(url)
+    response.raise_for_status()
+    if (response.json()['status'] == 'ZERO_RESULTS'):
+        return False
+    elif (response.json()['status'] != "OK"):
+        raise BaseException("Something wrong happened when reverse-geocoding this location:", latitude , longitude, "This is the error received", response.json()['status'])
+
+    address = response.json()["results"][0]["formatted_address"]
+    city = address.split(",")[1]
+
+    return city == city_name
+
+def download_satelite_image(latitude, longitude, key, fileName, width=640, height=640, zoom=17):
     f = open(fileName, "wb")
-    f.write(urllib.request.urlopen('https://maps.googleapis.com/maps/api/staticmap?center='                                    + str(latitude) +','+ str(longitude) + '&zoom=17&size=' +                                    str(width) + 'x' + str(height) + '&maptype=satellite&key=' + key)                                    .read())
+    f.write(urllib.request.urlopen('https://maps.googleapis.com/maps/api/staticmap?center='+ str(latitude) +','+ str(longitude) + '&zoom=17&size=' +str(width) + 'x' + str(height) + '&maptype=satellite&key=' + key).read())
     f.close()
     
+def download_map_image(latitude, longitude, key, fileName, width=640, height=640, zoom=17):
+    f = open(fileName, "wb")
+
+    f.write(urllib.request.urlopen('https://maps.googleapis.com/maps/api/staticmap?center=' + str(latitude) + ',' + str(longitude) + '&zoom=17&&size=640x640&style=element:labels|visibility:off&key=' + key).read())
+    f.close()
 
 
-
-def getPicWidthMeters(latitude, pic_width_pixels, zoom): 
+def get_pic_width_meters(latitude, pic_width_pixels, zoom):
     meters_per_pixel = 156543.03392 * math.cos(latitude * math.pi / 180) / math.pow(2, zoom) 
     pic_width_meters = meters_per_pixel * pic_width_pixels
     return pic_width_meters
@@ -20,7 +39,16 @@ def getPicWidthMeters(latitude, pic_width_pixels, zoom):
 # get coordinates of 2nd point, given a starting point, distance moved, and either moving east/west
 # latitude and longitude must be given in DEGREES
 # returns coordinates of new point in DEGREES
-def getSecondPointMovingEW(lat1, long1, distance, west= True): 
+def get_second_point(lat1, long1, distance, direction):
+    ' direction: int: 1 = north, 2 = east, 3 = south, 4 = west'
+    ret_value = {1: get_second_point_moving_NS(lat1, long1, distance, False),
+                 2: get_second_point_moving_EW(lat1, long1, distance, False),
+                 3: get_second_point_moving_NS(lat1, long1, distance, True),
+                 4: get_second_point_moving_EW(lat1, long1, distance, True)
+                 }
+    return ret_value[direction]
+
+def get_second_point_moving_EW(lat1, long1, distance, west= True):
     radius = 6371000 # radius of earth in meters  
     lat1 = math.radians(lat1)
     long1 = math.radians(long1)
@@ -28,14 +56,29 @@ def getSecondPointMovingEW(lat1, long1, distance, west= True):
     p2 = math.cos(lat1)
     quotient = p1/p2
     delta_longitude = 2*math.asin(quotient)
+    # print("inside fnctin", math.degrees(delta_longitude))
+    # print()
     if west:
         long2 = long1 - delta_longitude
     else:
         long2 = long1 + delta_longitude
-    
-    return math.degrees(long2)
+    lat2 = math.degrees(lat1) # doesn't change
+    long2 = math.degrees(long2)
+    return lat2, long2
 
 
+def get_second_point_moving_NS(lat1, long1, distance, south=True):
+    radius = 6371000  # radius of earth in meters
+    lat1 = math.radians(lat1)
+    long1 = math.radians(long1)
+    delta_latitude = distance/radius
+    if south:
+        lat2 = lat1 - delta_latitude
+    else:
+        lat2 = lat1 + delta_latitude
+    lat2 = math.degrees(lat2)
+    long2 = math.degrees(long1) # doesn't change
+    return lat2, long2
 
 # Inaccurate 2nd Coordinate Calculation 
 #theta measured counterclockwise from due east
@@ -53,20 +96,20 @@ def getSecondPointMovingEW(lat1, long1, distance, west= True):
 
 
 # Testing 
-key = "" 
-latitude = 41.878114
-longitude = -87.6298
-zoom = 17
-width = 640
-height = 640
-# downloadMapImage(latitude, longitude, key, "testimg1.png")
-
-
-distanceFromCenter = getPicWidthMeters(latitude, width, zoom)
-longitude = getSecondPointMovingEW(latitude, longitude, distanceFromCenter, west=False)
-print(distanceFromCenter)
-downloadMapImage(latitude, longitude, key, "testimg1.png")
-f = open("testimg2.png", "wb")
-f.write(urllib.request.urlopen('https://maps.googleapis.com/maps/api/staticmap?center='                                + str(latitude) +','+ str(longitude) + '&zoom=17&&size=640x640&maptype=satellite&key=' + key)                                .read())
-f.close()
+# key = "AIzaSyAVcfKag3qQqMrw-I3Lwg0c-UsOj1nx4UI"
+# latitude = 41.878114
+# longitude = -87.6298
+# zoom = 17
+# width = 640
+# height = 640
+# # downloadMapImage(latitude, longitude, key, "testimg1.png")
+#
+#
+# distance_from_center = get_pic_width_meters(latitude, width, zoom)
+# latitude, longitude = getSecondPointMovingEW(latitude, longitude, distanceFromCenter, west=False)
+# print(distanceFromCenter)
+# download_satelite_image(latitude, longitude, key, "testimg1.png")
+# f = open("testimg2.png", "wb")
+# f.write(urllib.request.urlopen('https://maps.googleapis.com/maps/api/staticmap?center='+ str(latitude) +','+ str(longitude) + '&zoom=17&&size=640x640&style=element:labels|visibility:off&key=' + key).read())
+# f.close()
 
---
+++ download_chicago.py
@@ -0,0 +1,70 @@
+from GoogleMapImageLib import download_map_image, get_second_point, get_pic_width_meters, is_within_city
+row_counter = 0
+key = ""
+image_coord_file = open("image_coords.txt", "w")
+
+def get_top_left_corner(center_lat, center_long, half_pic_width):
+    center_left_side = get_second_point(center_lat, center_long, half_pic_width, 4)
+    top_left_corner = get_second_point(center_left_side[0], center_left_side[1], half_pic_width, 1)
+    return top_left_corner
+
+
+def get_bot_right_corner(center_lat, center_long, half_pic_width):
+    center_right_side = get_second_point(center_lat, center_long, half_pic_width, 2)
+    bot_right_corner = get_second_point(center_right_side[0], center_right_side[1], half_pic_width, 3)
+    return bot_right_corner
+
+
+def download_right(lat, long, bound, width_pic, key):
+    half_pic_width = width_pic / 2
+    col_counter = 0
+
+    while long < bound:
+        if is_within_city(lat, long, "Chicago", key): # within the city ranges
+
+            filename = str(row_counter) + "_" + str(col_counter)
+            download_map_image(lat, long, key, str(row_counter) + "_" + str(col_counter))
+
+            TL_corner = get_top_left_corner(lat, long, half_pic_width)
+            BR_corner = get_bot_right_corner(lat, long, half_pic_width)
+            image_coord_file.write(filename + " %d %d %d %d\n" % (TL_corner[0], TL_corner[1], BR_corner[0], BR_corner[1]))
+
+            lat,long = get_second_point(lat, long, half_pic_width, 2) # move east by half of the pic's width
+            col_counter += 1
+        # else:
+            # print("still outside")
+
+
+
+def download_entire_lat(lat, long, right_bound, pic_width, key):
+    # download_left(lat, long, left_bound, key)
+    download_right(lat, long, right_bound, pic_width, key)
+    return
+
+
+# general setup
+upper_left_boundary_lat, upper_left_boundary_long = 42.017191, -87.819896
+
+lower_right_boundary_lat = 41.643764
+lower_right_boundary_long = -87.522456
+
+# picture width in meters changes everytime the latitude changes
+pic_width = get_pic_width_meters(upper_left_boundary_lat, 640, 17)
+starting_point = get_second_point(upper_left_boundary_lat, upper_left_boundary_long, pic_width / 2, 2)
+starting_point = get_second_point(starting_point[0], starting_point[1], pic_width/2, 3)
+print(starting_point)
+
+
+print(is_within_city(30.766072, -58.898850, "Chicago", key))
+
+while(starting_point[0] >= lower_right_boundary_lat):
+    # width changes everytime the latitude changes
+    pic_width = get_pic_width_meters(starting_point[0], 640, 17)
+    # download images for the current row
+    download_entire_lat(starting_point[0], starting_point[1], lower_right_boundary_long, pic_width, key)
+
+    # move down ; starting point will be the center of the leftmost square in the row below the current row
+    starting_point = get_second_point(starting_point[0], starting_point[1], pic_width/2, 3)
+    row_counter += 1
+
+image_coord_file.close()
